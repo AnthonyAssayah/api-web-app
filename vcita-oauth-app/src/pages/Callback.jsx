913eb8ab242af5c5b4608980ee13f04d8970888e
Anthony <anthonyassayah14@gmail.com>
2024-11-14 12:50:03 +0200

    useEffect for rendering

--- vcita-oauth-app/src/pages/Callback.jsx
+++ vcita-oauth-app/src/pages/Callback.jsx
@@ -6,6 +6,34 @@ const CallbackPage = () => {
   const [accessToken, setAccessToken] = useState('');
   const [clients, setClients] = useState([]);
 
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+
+    if (code) {
+      // Exchange the authorization code for an access token
+      const clientId = "bcbaf8cd30eea62d65a24812529c214b3f32ad29cf0ad1fb9de1680d30ad6aca";
+      const clientSecret = "ffa0326272bb49d447b5b77168b6e3ffc81fc34abbc6fe08f9c0ea0ea2c58c34";
+      const redirectUri = "https://my-vcita-playground.web.app/oauth";
+      
+      axios.post('https://app.vcita.com/oauth/token', {
+        code,
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code',
+      })
+      .then(response => {
+        const token = response.data.access_token;
+        setAccessToken(token);
+        // Fetch clients once the token is obtained
+        fetchClients(token);
+      })
+      .catch(error => console.error('Error exchanging code for token:', error));
+    }
+  }, []);
+
+
 
   return (
     <div>
